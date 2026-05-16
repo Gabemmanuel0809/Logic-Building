37d918c14ca5f0bed0581f4cb551ef88a5ef7f3e
Gabriel Emmanuel P. Domondon <gabemmanuel468@gmail.com>
2026-05-16 14:03:42 +0800

    Create JoinArr.go

---
+++ JoinArr.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+  var users = [...]string{"John", "Bob", "Peter"}
+	var orders = [...]string{"Burger", "Fries", "Doughnout"}
+
+  // Joining two arrays in a map
+	var usr_ordr = make(map[string]string)
+	usr_ordr[users[0]] = orders[2]
+	usr_ordr[users[1]] = orders[0]
+	usr_ordr[users[2]] = orders[1]
+
+	for key, value := range usr_ordr {
+		fmt.Println(key, ":", value)
+	}
+}
